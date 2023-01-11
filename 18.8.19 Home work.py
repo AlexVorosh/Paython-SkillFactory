b7c9ced10f5b9de90dcdb3457fac6f6623e07378
Alexander Voroshilov <119477867+AlexVorosh@users.noreply.github.com>
2023-01-11 16:07:25 +0700

    Add files via upload

---
+++ 18.8.19 Home work.py
@@ -0,0 +1,13 @@
+tickets_count = int(input('Введите количество билетов: '))
+sum = 0
+for ticket in range (1, tickets_count + 1):
+    age = int(input(f'Введите возраст посетителя по билету {ticket}: '))
+    if 18 <= age < 25:
+        sum += 990
+    elif age >= 25:
+        sum += 1390
+if tickets_count > 3:
+    sum = sum - sum*0.1
+    print(f'Скидка 10% при заказе более 3-х билетов: {sum} руб.')
+
+print(f'Сумма к оплате: {sum} руб.')
